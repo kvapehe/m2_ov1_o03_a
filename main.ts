93a3f10d6d82b541de9ea183de06ae1dd8b577ef
kvapehe <33888633+kvapehe@users.noreply.github.com>
2021-09-12 20:47:00 +0200

    Initiell versjon

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+/**
+ * Eksperimentere med github hyppig commit
+ */
+basic.forever(function () {
+    music.ringTone(175)
+    basic.pause(100)
+    music.rest(music.beat(BeatFraction.Whole))
+    basic.pause(9000)
+})
